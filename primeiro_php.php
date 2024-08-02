b04b9fc473d686819be10bdc145d846b4dec595b
Sch1mith <165226331+Sch1mith@users.noreply.github.com>
2024-08-01 22:31:52 -0300

    Add files via upload

---
+++ primeiro_php.php
@@ -0,0 +1,45 @@
+<?php
+	echo "<!DOCTYPE html>
+
+    <html>
+        <head>
+            <title>Exercicio php</title>
+            <style>
+            body{
+                background-color: pink;
+            }
+    
+            h1{
+                font-family: sans-serif;
+                font-size: 30px;
+                color: blue;
+                text-decoration: underline;
+                text-decoration-style: wavy;
+                text-decoration-color: #FFCC00;
+                text-align: center;
+            }
+            p{
+                padding: 15px;
+                margin: 15px;
+                border: 2px;;
+                border-style: groove;
+                font-family: arial;
+                text-align: center;
+                color: blue;
+            }
+            </style>
+    
+        </head>
+    
+        <body>
+            <h1>php é legal</h1>
+            <p> PHP (um acrônimo recursivo para 'PHP: Hypertext Preprocessor', 
+            originalmente Personal Home Page) é uma linguagem interpretada livre, 
+            usada originalmente apenas para o desenvolvimento de aplicações presentes 
+            e atuantes no lado do servidor, capazes de gerar conteúdo dinâmico na World Wide Web.
+            Figura entre as primeiras linguagens passíveis de inserção em documentos HTML, 
+            dispensando em muitos casos o uso de arquivos externos para eventuais processamentos de dados</p>
+        </body>
+    </html>";
+
+?>
